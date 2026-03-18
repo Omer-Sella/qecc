c1253c08db19b1ef2dcab10b7ae48758518ebbe7
Omer (Homer) Sella <osella@cx3-3-11.cx3.hpc.ic.ac.uk>
2026-03-18 15:41:02 +0000

    hpc commit

--- src/qecc/__init__.py
+++ src/qecc/__init__.py
@@ -5,7 +5,7 @@ QECC (Quantum Error Correction Code) package initialization.
 
 __version__ = '0.1.2'
 __author__ = 'Omer Sella'
-__all__ = ["memBP", "minSum", "polynomialCodes", "funWithMatrices", "osd", "logicals", "polynomialCodes", "gf4", "solver"]
+__all__ = ["memBP", "minSum", "polynomialCodes", "funWithMatrices", "osd", "logicals", "polynomialCodes", "gf4", "solver", "arithmetic"]
 PACKAGE_NAME = "qecc"
 
 from gymnasium.envs.registration import register
